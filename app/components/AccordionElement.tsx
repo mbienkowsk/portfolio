f03aacad961a1507525416ca3f6787e2c7bb4b07
maksym <01178511@pw.edu.pl>
2024-02-25 21:18:23 +0100

    smooth scrolling, modify accordion element padding to margin ratio

--- app/components/AccordionElement.tsx
+++ app/components/AccordionElement.tsx
@@ -13,7 +13,7 @@ export default function AccordionElement({title, content}: {
 
     return (
         <div>
-            <button className="text-5xl pt-24 pb-10 text-on_hover flex gap-16 hover:text-text_primary transition-opacity duration-200" onClick={toggleContent}>
+            <button className="text-5xl w-full mt-20 pt-4 mb-6 pb-4 text-on_hover flex gap-16 hover:text-text_primary transition-all duration-200" onClick={toggleContent}>
                 <div
                     className={`${showContent ? 'rotate-90' : 'rotate-0'} transition-transform`}>&gt;   </div>
                 <div>{title}</div>
