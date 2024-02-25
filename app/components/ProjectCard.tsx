5d2d00e46684025417f5964b53317bf644992387
maksym <01178511@pw.edu.pl>
2024-02-25 21:15:27 +0100

    change project card button to link, add url prop

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,23 +1,31 @@
 import Image from "next/image";
+import Link from "next/link";
 
 
-export default function ProjectCard({imgPath, title, description}: {
+export default function ProjectCard({imgPath, title, description, url = ""}: {
     imgPath: string,
     title: string,
     description: string
+    url: string
 }) {
-    const PROJECT_CARD_WIDTH = 450;
+    const PROJECT_CARD_WIDTH = 355;
     const PROJECT_CARD_HEIGHT = Math.floor(.67 * PROJECT_CARD_WIDTH);
     return <div className="relative group">
         <Image src={imgPath}
-               alt="Image depicting a chessboard linking to a chess project."
+               alt="Image depicting one of my projects" //todo: separate alts for all cards
                width={PROJECT_CARD_WIDTH}
                height={PROJECT_CARD_HEIGHT}
-               className="rounded-xl border-text_primary border-2 group-hover:scale-110 duration-200 group-hover:opacity-15"></Image>
-        <div className="absolute top-0 left-0 opacity-0 group-hover:opacity-100 delay-200 w-full h-full text-center">
-            <h1 className="text-3xl pt-6 bold">{title}</h1>
-            <p className="text-xl">{description}</p>
-            <button className="mt-10 hover:text-on_hover duration-200 text-2xl">Check it out</button>
+               className="rounded-xl border-text_primary border-2 group-hover:scale-110 duration-200 group-hover:opacity-15">
+        </Image>
+        <div
+            className="absolute top-0 left-0 opacity-0 w-full h-full group-hover:opacity-100 delay-200 text-center">
+            <div>
+                <h1 className="text-3xl bold pt-2 pb-2">{title}</h1>
+                <p className="text-xl">{description}</p>
+            </div>
+            <Link href={url}
+                  className="hover:text-on_hover duration-200 text-2xl absolute bottom-0 left-0 text-center w-full">Check
+                it out</Link>
         </div>
     </div>
 }
