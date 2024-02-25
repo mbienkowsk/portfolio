557505bd1b1e048399bc931b01b6effa341d0b4c
maksym <01178511@pw.edu.pl>
2024-02-25 20:56:43 +0100

    another version of the navbarm it's not used anyway as of now

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,32 @@
 import Link from "next/link";
+import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEnvelope, faEnvelopeOpen} from "@fortawesome/free-solid-svg-icons";
+import {useRef} from "react";
 
 export default function Navbar() {
-    return <nav className="flex justify-between w-full text-gray-400 fixed bg-background">
-        <Link href="/#about"
-              className="text-xl p-6 hover:bg-gray-700 rounded-b w-full text-center hover:text-on_hover">ABOUT
-            ME</Link>
-        <Link href="/#projects"
-              className="text-xl p-6 hover:bg-gray-700 rounded-b w-full text-center hover:text-on_hover">PROJECTS</Link>
-        <Link href="/#resume"
-              className="text-xl p-6 hover:bg-gray-700 rounded-b w-full text-center hover:text-on_hover">RESUME</Link>
-        <Link href="/#contact"
-              className="text-xl p-6 hover:bg-gray-700 rounded-b w-full text-center hover:text-on_hover">CONTACT</Link>
+    const ICON_SIZE = 30
+    // return <nav className={"flex justify-between w-full text-gray-400 fixed bg-background"}>
+    //     <Link href="https://github.com/mbienkowsk"
+    //           className="text-xl p-6 hover:bg-gray-700 rounded-b w-full text-center hover:text-on_hover duration-200">github</Link>
+    //     <Link href="https://www.linkedin.com/in/m-bienkowski/"
+    //           className="text-xl p-6 hover:bg-gray-700 rounded-b w-full text-center hover:text-on_hover duration-200">linkedin</Link>
+    //     <Link href="mailto:bienkowski.maksym@gmail.com"
+    //           className="text-xl p-6 hover:bg-gray-700 rounded-b w-full text-center hover:text-on_hover duration-200">mail</Link>
+    // </nav>
+
+    return <nav className={"flex gap-8 text-gray-400 fixed bg-background"}>
+        <Link href="https://github.com/mbienkowsk"
+              className="text-xl px-6 pt-6 hover:bg-gray-700 rounded-b w-full text-center hover:text-on_hover duration-200"><FontAwesomeIcon
+            icon={faGithub} width={ICON_SIZE}/></Link>
+        <Link href="https://www.linkedin.com/in/m-bienkowski/"
+              className="text-xl px-6 pt-6 hover:bg-gray-700 rounded-b w-full text-center hover:text-on_hover duration-200"><FontAwesomeIcon
+            icon={faLinkedin} width={ICON_SIZE}/></Link>
+        <Link href="mailto:bienkowski.maksym@gmail.com"
+              className="text-xl px-6 pt-6 hover:bg-gray-700 rounded-b w-full text-center hover:text-on_hover duration-200 group">
+            <FontAwesomeIcon className="group-hover:hidden group-hover:opacity-0" icon={faEnvelope}
+                             width={ICON_SIZE + 3}/>
+            <FontAwesomeIcon className="hidden group-hover:block" icon={faEnvelopeOpen} width={ICON_SIZE}/>
+        </Link>
     </nav>
 }
