92767dbfb5dc4d455f8cc2c0b9469fce317c3dbc
maksym <01178511@pw.edu.pl>
2024-03-14 18:15:49 +0100

    add analytics component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { jbmono } from "@/styles/fonts";
+import { Analytics } from "@vercel/analytics/react";
 
 
 export const metadata: Metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({ children, }: Readonly<{
                 <div className="flex justify-center">
                     <div className="w-11/12 xl:w-10/12 2xl:w-3/4 3xl:w-1/2">
                         {children}
+                        <Analytics />
                     </div>
                 </div>
             </body>
